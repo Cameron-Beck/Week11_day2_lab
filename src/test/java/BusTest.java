58e78135f0db4510a6c2e5bb2f6f6959eeff889c
Cameron <cameron.beckit@gmail.com>
2020-01-07 14:29:02 +0000

    adds basic test

---
+++ src/test/java/BusTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BusTest {
+
+    private Bus bus;
+    private Passanger passanger;
+
+    @Before
+    public void before() {
+        bus = new Bus("Ocean Terminal");
+        passanger = new Passanger();
+    }
+        @Test
+        public void hasDestination(){
+            assertEquals("Ocean Terminal", bus.getDestination());
+        }
+
+        @Test
+        public void hasCapacity(){
+            assertEquals(5, bus.getCapacity());
+        }
+
+        @Test
+        public void countPassangers(){
+        assertEquals(0, bus.countPasangers());
+        }
+
+}
